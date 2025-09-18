f895ee1b4f5c4b64e82828b4a8c5c53cc5b30f01
abhu-baba <offactsking@gmail.com>
2025-09-18 11:46:45 +0530

    Add files via upload

---
+++ calculatour.py
@@ -0,0 +1,21 @@
+'''def grades(marks):
+    if marks <= 60:
+        print("d")
+    elif marks <=  80:
+        print("c")
+    elif marks <= 85:
+        print("b")
+    else:
+        print("a")
+grades(56)'''
+
+def celculator(a,b,operater):
+    if operater =="+":
+        print(a+b)
+    elif marks >= 75:
+        print("b")
+    elif marks >=60:
+        print("c")
+    else:
+        print("d")
+celculator (45,80,"+")
